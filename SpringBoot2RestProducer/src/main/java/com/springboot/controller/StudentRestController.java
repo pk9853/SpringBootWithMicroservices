e3a2bdeea10499eb001e289603de8cf2c8948683
prasantakumar.b <prasantakumar.b@VLSYS52-20.virinchi.com>
2022-01-21 10:34:02 +0530

    new project

--- SpringBoot2RestProducer/src/main/java/com/springboot/controller/StudentRestController.java
+++ SpringBoot2RestProducer/src/main/java/com/springboot/controller/StudentRestController.java
@@ -1,8 +1,13 @@
 package com.springboot.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +35,21 @@ public class StudentRestController {
 		return ResponseEntity.ok(new Student(10, "A", 200.0));
 
 	}
-
+	@GetMapping("/showd")
+	public ResponseEntity<List<Student>> showMsgD() {
+		System.out.println("From SHOWD");
+		return ResponseEntity.ok(
+				Arrays.asList(
+						new Student(10, "A", 200.0),
+						new Student(11,"B",300.0),
+						new Student(12,"C",400.0)
+						));
+	}
+	
+	@PostMapping("/savea")
+	public  ResponseEntity<String> saveDataA(@RequestBody Student student)
+	{
+		System.out.println("FROM SAVE#A"+student);
+		return ResponseEntity.ok(student.toString());
+	}
 }
